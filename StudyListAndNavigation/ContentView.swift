51f597131cf8e4d5bec96240302454aa849f7e35
YimKeul <leesjun29@gmail.com>
2023-03-16 01:33:23 +0900

    feat : add List

--- StudyListAndNavigation/ContentView.swift
+++ StudyListAndNavigation/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List{
-            Text("Wash the car ")
-            Text("Vacuum house ")
-            Text("Pick up kids from school bus @ 3pm ")
-            Text("Auction the kids on eBay ")
-            Text("Order Pizza for dinner ")
+        VStack{
+            List{
+                Text("Wash the car ")
+                Text("Vacuum house ")
+                Text("Pick up kids from school bus @ 3pm ")
+                Text("Auction the kids on eBay ")
+                Text("Order Pizza for dinner ")
+            }
+            
+            List{
+                HStack{
+                    Image(systemName: "trash.circle.fill")
+                    Text("Take out hte trash")
+                }
+                HStack{
+                    Image(systemName: "person.2.fill")
+                    Text("Pick up the kids")
+                }
+                HStack{
+                    Image(systemName: "car.fill")
+                    Text("Wash the car")
+                }
+            }
         }
+        
     }
 }
 
